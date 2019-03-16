832fdd3011714c43e5f032bba7ae23bfa310cf1f
Domines <719480072@qq.com>
2019-03-16 23:59:57 +0800

    add pymongo

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask
 from flask_restful import Api
+from flask_pymongo import PyMongo
 from resources.foo import Foo
 from resources.papers import Papers
 
 app = Flask(__name__)
 api = Api(app)
+
+app.config.update(
+    MONGO_URI='mongodb://localhost:27017/mean-dev'
+)
+mongo = PyMongo(app)
+
 
 api.add_resource(Foo, '/Foo')
 
