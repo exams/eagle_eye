51260271b1b883ef2a47f8190945fbbb5b68cba0
719480072@qq.com <719480072@qq.com>
2019-03-19 23:23:54 +0800

    尝试使用pycharm提交

--- resources/foo.py
+++ resources/foo.py
@@ -4,7 +4,7 @@ from flask_restful import Resource
 class Foo(Resource):
 
     def get(self):
-        return {'hello': 'world'}
+        return {'hello': 'world', "中国": "加油"}
 
     def post(self):
         pass
--- resources/tasks.py
+++ resources/tasks.py
@@ -1,10 +1,21 @@
-from flask_restful import Resource
+from flask_restful import fields, marshal_with, reqparse, Resource
+from app import mongo
 
+
+post_parser = reqparse.RequestParser()
+task_fields = {
+    'id': fields.Integer,
+    'name': fields.String,
+    'process': fields.Integer
+}
 
 class Tasks(Resource):
 
     def get(self):
         pass
 
+    @marshal_with(task_fields)
     def post(self):
+        args = post_parser.parse_args()
+        mongo.db.tasks.save()
         pass
